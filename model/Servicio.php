55f999d3124f49cbdab16d7813fe4c9d4b0f6057
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-03-13 20:55:20 -0600

    Create Servicio.php

---
+++ model/Servicio.php
@@ -0,0 +1,19 @@
+<?php
+  class Servicio{
+		public $id;
+		public $tiempo;
+		public $descripcion;
+		
+		
+		
+			function __construct($id, $tiempo, $descripcion){
+			$this-> id = $id;
+			$this -> tiempo = $tiempo;
+			$this -> descripcion = $decripcion;
+			
+				
+                   
+		}
+
+	}
+?>
