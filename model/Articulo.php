c6af6ab3e465edd3ad587002e44ee6e8284d8237
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-03-13 20:53:12 -0600

    Create Articulo.php

---
+++ model/Articulo.php
@@ -0,0 +1,19 @@
+<?php
+  class Articulo{
+		public $id;
+		public $nombre;
+		public $descripcion;
+		public $precio_venta;
+		
+		
+			function __construct($id, $nombre, $descripcion,$precio_venta){
+			$this-> id = $id;
+			$this -> nombre = $nombre;
+			$this -> descripcion = $decripcion;
+			$this -> precio_venta = $precio_venta;
+				
+                   
+		}
+
+	}
+?>
