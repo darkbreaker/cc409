1a85a29bf53113917d003a71cf72bafb52bf5670
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-03-13 21:36:16 -0600

    Update ArticuloBSS.php

--- model/ArticuloBSS.php
+++ model/ArticuloBSS.php
@@ -4,6 +4,16 @@
 		public $nombre;
 		public $descripcion;
 		public $precio_venta;
-
+         agregarArticulo($nombre, $descripcion, $precio_compra, $precio_venta){
+        }
+         buscarArticulo($idArticulo){
+         }
+         eliminarArticulo($idArticulo){
+         }
+         ReservarArticulo($idArticulo, $idUsuario){
+         }
+         listar(){
+         }
+         
 	}
 ?>
