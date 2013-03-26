adb3e46b20b1e194101ea6ccd36ab92c77ac2148
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-03-26 12:20:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,38 +3,35 @@
  *@package veterinariaweb
  //index principal
  */
- include('controller/UsuarioCtl.php');
- include('controller/CitaCtl.php');
- include('controller/NotaVentaCtl.php');
- include('controller/PedidoCtl.php');
- include('controller/ServicioCtl.php');
- include('controller/ArticuloCtl.php');
- include('controller/LogCtl.php');
- 
- 
  switch($_REQUEST['accion']){
 	case 'usuario':
+		include('controller/UsuarioCtl.php');
 		$controlador = new UsuarioCtl();
 		break;
 	case 'cita':
+		include('controller/CitaCtl.php');
 		$controlador = new CitaCtl();
-	break;
+	        break;
 	case 'notaVenta':
+		include('controller/NotaVentaCtl.php');
 		$controlador = new NotaVentaCtl();
-	break;
+           	break;
 	case 'pedido':
+		include('controller/PedidoCtl.php');
 		$controlador = new PedidoCtl();
-	break;
+        	break;
 	case 'servicio':
+		include('controller/ServicioCtl.php');
 		$controlador = new ServicioCtl();
-	break;
+	        break;
 	case 'articulo':
+	        include('controller/ArticuloCtl.php');
 		$controlador = new ArticuloCtl();
-	break;
-	case 'log':
-		$controlador = new LogCtl();
 		break;
-	
+	case 'log':
+		include('controller/LogCtl.php');
+		$controlador = new LogCtl();
+		break;	
  }
 
  $controlador->ejecutar();
