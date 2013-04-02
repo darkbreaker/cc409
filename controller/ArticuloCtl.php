a3ff6eee14daf66debc4522558281196a31fcf4a
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-04-02 14:13:28 -0600

    Update ArticuloCtl.php

--- controller/ArticuloCtl.php
+++ controller/ArticuloCtl.php
@@ -12,30 +12,35 @@ include_once('model/ArticuloBss.php');
 		function ejecutar(){
 			//si no tengo parametros se listan los Articulos
 			$hacer=$_REQUEST['hacer'];
-			
-			if(!isset($hacer) ){
+			$id=$_REQUEST['id'];
+			$nombre=$_REQUEST['nombre'];
+			$descripcion=$_REQUEST['descripcion'];
+			$precio_venta=$_REQUEST['precio_venta'];
+			$idUsuario=$_REQUEST['idUsuario'];
+		
+		         	if(!isset($hacer) ){
 				$Articulo = $this->modelo-> listar();
 				//vista del resultado
 				include('view/listarArticuloView.php');
 			} else switch($hacer){
 				case 'agregar':
-					$Articulo=$this->modelo->agregarArticulo($_REQUEST['nombre'],$_REQUEST['descripcion'],$_REQUEST['precio_venta']) ;
+					$Articulo=$this->modelo->agregarArticulo($nombre, $descripcion, $precio_venta) ;
 					include('view/agregarArticuloView.php');
 					break;
 				case 'consultar':
-					$Articulo=$this->modelo->consultarArticulo($_REQUEST['id']);
+					$Articulo=$this->modelo->consultarArticulo($id);
 					include('view/consultarArticuloView.php');
 					break;
 				case 'eliminar':
-				$Articulo=$this->modelo->eliminarArticulo($_REQUEST['idUsuario']);
+				$Articulo=$this->modelo->eliminarArticulo($idUsuario);
 					include('view/eliminarArticuloView.php');
 					break;
 				case 'modificar':
-					$Articulo=$this->modelo->modificarArticulo($_REQUEST['nombre'],$_REQUEST['descripcion'],$_REQUEST['precio_venta']) ;
+					$Articulo=$this->modelo->modificarArticulo($nombre, $descripcion, $precio_venta) ;
 					include('view/modificarArticuloView.php');
 					break;
 				case 'filtrar':
-			$Articulo=$this->modelo->filtrarArticulo($_REQUEST['descripcion']);
+			$Articulo=$this->modelo->filtrarArticulo($descripcion);
 					include('view/filtrarArticuloView.php');
 					break;
 				case 'listar':
