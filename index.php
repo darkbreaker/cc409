f85596475b7c9453396e18fbf1c2b2f6c841fd39
franz navarro <f2_@hotmail.com>
2013-05-22 17:12:29 -0500

    modificado
    para funcionar en el alanturing

--- index.php
+++ index.php
@@ -1,4 +1,51 @@
 <?php
-	include("perrosygatos/index.php");
+/**
+ *@package veterinariaweb
+ //index principal
+ */
+ $accion=$_REQUEST['accion'];
+ if(!isset($accion)){
+	include('controller/DefaultCtl.php');
+	$controlador = new DefaultCtl();
+	}else
 	
+ switch($_REQUEST['accion']){
+	case 'usuario':
+		include('controller/UsuarioCtl.php');
+		$controlador = new UsuarioCtl();
+		break;
+	case 'cita':
+		include('controller/CitaCtl.php');
+		$controlador = new CitaCtl();
+	        break;
+	case 'notaVenta':
+		include('controller/NotaVentaCtl.php');
+		$controlador = new NotaVentaCtl();
+           	break;
+	case 'pedido':
+		include('controller/PedidoCtl.php');
+		$controlador = new PedidoCtl();
+        	break;
+	case 'servicio':
+		include('controller/ServicioCtl.php');
+		$controlador = new ServicioCtl();
+	        break;
+	case 'articulo':
+	        include('controller/ArticuloCtl.php');
+		$controlador = new ArticuloCtl();
+		break;
+	case 'log':
+		include('controller/LogCtl.php');
+		$controlador = new LogCtl();
+		break;
+	default:
+		include('controller/DefaultCtl.php');
+		$controlador = new DefaultCtl();
+ }
+
+ $controlador->ejecutar();
+
+
+
+
 ?>
