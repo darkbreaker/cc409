b8471b4849fb3a08833f25bcc0519b0d838de4f2
Alejandro Hinojosa <al_xsnake@hotmail.com>
2013-06-04 20:28:08 -0500

    updte

--- view/js/jasontable.js
+++ view/js/jasontable.js
@@ -1,4 +1,12 @@
-function crearTabla(objArray, dir) {
+function crearTabla(objArray, theme, enableHeader) {
+    // set optional theme parameter
+    if (theme === undefined) {
+        theme = 'mediumTable'; //default theme
+    }
+ 
+    if (enableHeader === undefined) {
+        enableHeader = true; //default enable headers
+    }
  
     // If the returned data is an object do nothing, else try to parse
     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
@@ -6,24 +14,20 @@ function crearTabla(objArray, dir) {
     var str = '<table class="table-hover" id="jtable" name="jtable">';
  
     // table head
+    if (enableHeader) {
         str += '<thead><tr>';
         for (var index in array[0]) {
             str += '<th scope="col">' + index + '</th>';
         }
         str += '</tr></thead>';
-    
+    }
  
     // table body
     str += '<tbody>';
     for (var i = 0; i < array.length; i++) {
         str += (i % 2 == 0) ? '<tr class="alt">' : '<tr>';
-		var id=0;
         for (var index in array[i]) {
-			if(id==0)
-				str += '<td><a '+ "onmouseover=\"this.innerHTML = 'Click para reservar'\" onmouseout=\"this.innerHTML = '"+array[i][index] +"'\" "  + ' href="index.php?accion='+dir+array[i][index] +'">' + array[i][index] + '</a></td>';
-			else
-				str += '<td>' + array[i][index] + '</td>';
-				id++;
+            str += '<td>' + array[i][index] + '</td>';
         }
         str += '</tr>';
     }
@@ -31,4 +35,3 @@ function crearTabla(objArray, dir) {
     str += '</table>';
     return str;
 }
-
